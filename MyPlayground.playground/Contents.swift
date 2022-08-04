030e4f48d0bbeb4a5e58b5617f339b2052d00de0
Armağan Gök <1armagangok@gmail.com>
2022-08-04 12:00:15 +0300

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+/// immutable variables cannot be assigned while run time but can be assgined while compile time.
+/// let operand is used to declare an immutable variable.
+let pi = 3.14
+
+
+/// mutable variables can be assigned while run time.
+/// var operand is used to declare an mutable variable.
+var number = 3
